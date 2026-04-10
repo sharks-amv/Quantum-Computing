e94b27ce5323af36dcaeec5917f9d00aefd55988
enkay1309 <kashyapnehal13.09@gmail.com>
2026-04-10 10:35:38 +0530

    qiskit_aer changes

--- evemod.py
+++ evemod.py
@@ -1,4 +1,6 @@
-
+import random
+from qiskit_aer import AerSimulator
+from qiskit import transpile
 
 def eve_bases_generator(n):
     return [random.randint(0,1) for _ in range(n)]
@@ -6,25 +8,24 @@ def eve_bases_generator(n):
 def eve_attack(qc, n, attack_prob):
     eve_bases = eve_bases_generator(n)
 
-    backend = Aer.get_backend('qasm_simulator')
+    backend = AerSimulator()
 
     for i in range(n):
         if random.random() < attack_prob:
 
-          
             if eve_bases[i] == 1:
                 qc.h(i)
 
-      
             qc.measure(i, i)
-            job = execute(qc, backend, shots=1)
+
+            transpiled = transpile(qc, backend)
+            job = backend.run(transpiled, shots=1)
+
             res = list(job.result().get_counts().keys())[0]
             b = int(res[n-1-i])
 
-        
             qc.reset(i)
 
-          
             if eve_bases[i] == 0:
                 if b == 1:
                     qc.x(i)
